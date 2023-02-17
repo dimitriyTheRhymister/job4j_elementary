696f3960ac53460da9547b1323abb416f43d1f7b
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-17 19:35:56 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,53 @@
+package ru.job4j.array;
+
+import static java.lang.Character.*;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = true;
+        if (name.isEmpty()) {
+            valid = false;
+        } else if (isUpperCase(name.codePointAt(0))) {
+            valid = false;
+        }
+        if (isNumeric(name)) {
+            valid = false;
+        }
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!(isSpecialSymbol(code) || isLowerLatinLetter(code) || isUpperLatinLetter(code) || isDigit(code))) {
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isNameValid(""));
+        System.out.println(isNameValid("A$F8_d"));
+        System.out.println(isNameValid("a$F8_d"));
+        System.out.println(isNameValid("123"));
+    }
+}

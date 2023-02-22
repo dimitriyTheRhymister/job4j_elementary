2a8f4f3c1806d01a4108007835bb58c4a614f4f1
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-22 22:10:36 +0300

    3. Перегрузить метод max для трех чисел [#122644]_ref

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,8 +5,20 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int x, int y, int z) {
+        return max(max(x, y), z);
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return max(max(a, b), max(c, d));
+    }
+
     public static void main(String[] args) {
-        int max = Max.max(11, 8);
-        System.out.println(max);
+        int max2 = Max.max(11, 8);
+        System.out.println(max2);
+        int max3 = Max.max(11, 8, 15);
+        System.out.println(max3);
+        int max4 = Max.max(11, 8, 15, 154);
+        System.out.println(max4);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,25 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax11To8To15Then15() {
+        int x = 11;
+        int y = 8;
+        int z = 15;
+        int result = Max.max(x, y, z);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax11To8To15To154Then154() {
+        int a = 11;
+        int b = 8;
+        int c = 15;
+        int d = 154;
+        int result = Max.max(a, b, c, d);
+        int expected = 154;
+        assertThat(result).isEqualTo(expected);
+    }
 }

cf56edbcf121f798509295981eda633f5cf832e3
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-22 19:58:42 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]_ref

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,33 +7,27 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when42to22then2() {
         double expected = 2;
-        int x1 = 4;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(4, 2);
+        Point b = new Point(2, 2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when825to422then5() {
         double expected = 5;
-        int x1 = 8;
-        int y1 = 25;
-        int x2 = 4;
-        int y2 = 22;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(8, 25);
+        Point b = new Point(4, 22);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }

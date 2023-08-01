8d85c3ee378d9446b98c133cc89012009b53cca4
Dmitriy Kupreev <d3_october@yahoo.com>
2023-08-01 20:38:53 +0300

    fix pom.xml - remove <encoding>UTF-8</encoding> from <configuration> .... </configuration> + add 2 stringы in main.java

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello New world");
         System.out.println("Привет мир!");
+        System.out.println("Привет Новый мир!");
     }
 }

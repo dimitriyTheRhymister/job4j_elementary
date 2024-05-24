1d63cd2b4eea13dbef08fbfeba9223121a382dad
Dmitriy Kupreev <d3_october@yahoo.com>
2024-05-24 19:32:32 +0300

    simply add to git Temp

---
+++ src/main/java/ru/job4j/Temp.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class Temp {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = map::put;
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
+//        System.out.println(map);
+
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+//        System.out.println(strings);
+
+        Consumer<String> con = System.out::println;
+        Function<String, String> func = String::toUpperCase;
+        for (String s : strings) {
+            con.accept(func.apply(s));
+        }
+    }
+}
